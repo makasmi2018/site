f56af5c111c66caab283a971be1cd423ffe0b080
kasmi mohammed amine <makasmi.ensao@gmail.com>
2018-03-30 20:45:43 +0100

    mesgae

--- pfe/public/js/js_accueil.js
+++
@@ -1,9 +0,0 @@
-var image = 4;
-var k = 1;
-function suivant() {if (k === image) { k = 0; }
-        var pos_slider = k * (-740);
-                     
-         pos_slider = pos_slider + "px";
-                     
-         document.getElementById('contenu').style.marginLeft = pos_slider; k++; }
-
